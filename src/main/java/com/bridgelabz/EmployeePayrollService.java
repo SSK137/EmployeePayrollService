f6f2421d8d56b36da0c65c4446fea59a03656d0c
SSK137 <saurabhkodam@gmail.com>
2022-05-24 15:07:40 +0530

    Employee Payroll Service: Welcome Message

---
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -0,0 +1,6 @@
+package com.bridgelabz;
+public class EmployeePayrollService {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Payroll Service using File Handling ");
+    }
+}
